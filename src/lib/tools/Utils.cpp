4e4a76c471a535c237a9aa41c5c108e1478a15fc
Simon <Simon>
2018-03-17 14:12:12 +0100

    ADDED utils.cpp and check for very low distance

---
+++ src/lib/tools/Utils.cpp
@@ -0,0 +1,67 @@
+#include "Utils.hpp"
+
+#include "entity/Move.hpp"
+#include "entity/Entity.hpp"
+
+namespace path {
+
+namespace tools {
+
+/**
+ * @brief will check if the comination of two moves is legal.
+ * If not will update vectors in order to have maximum move
+ * without collision
+ **/
+bool checkAndUpdateMoves(Move & move1_p, Move & move2_p)
+{
+    double const & r1(move1_p.getEntity().getRadius());
+    double const & r2(move2_p.getEntity().getRadius());
+    Vector<double> const & p1(move1_p.getEntity().getPosition());
+    Vector<double> const & p2(move2_p.getEntity().getPosition());
+    Vector<double> & v1(move1_p.getVector());
+    Vector<double> & v2(move2_p.getVector());
+    double dx(p1.x-p2.x);
+    double dy(p1.y-p2.y);
+    double dvx(v1.x-v2.x);
+    double dvy(v1.y-v2.y);
+    double R(r1+r2);
+
+    double a(dvx*dvx + dvy*dvy);
+    double b(2*dx*dvx + 2*dy*dvy);
+    double c(dx*dx+dy*dy-R*R);
+
+    std::cout<<"a="<<a<<std::endl;
+    std::cout<<"b="<<b<<std::endl;
+    std::cout<<"c="<<c<<std::endl;
+
+    // Check if distance is very low and if it decreases
+    if ( c <= 0.01 && b <= 0.0 )
+    {
+        // set no move
+        move1_p.getVector() = 0.0 * move1_p.getVector();
+        move2_p.getVector() = 0.0 * move2_p.getVector();
+        return true;
+    }
+
+    double delta(b*b-4*a*c);
+    if ( delta >= 0.0 )
+    {
+        double t((-b-f_sqrt(delta))/2.0/a);
+        if ( t>= 0.0 && t<= 1.0 ) {
+            move1_p.getVector() = t * move1_p.getVector();
+            move2_p.getVector() = t * move2_p.getVector();
+            return true;
+        } else
+        {
+            return false;
+        }
+    } else
+    {
+        return false;
+    }
+}
+
+
+}
+
+}
--- src/lib/tools/Utils.hpp
+++ src/lib/tools/Utils.hpp
@@ -4,6 +4,8 @@
 
 namespace path {
 
+class Move;
+
 namespace tools {
 
 // fast sqrt
@@ -19,42 +21,7 @@ double inline __declspec (naked) __fastcall f_sqrt(double n)
  * If not will update vectors in order to have maximum move
  * without collision
  **/
-bool checkAndUpdateMoves(Move & move1_p, Move & move2_p)
-{
-    double const & r1(move1_p.getEntity().getRadius());
-    double const & r2(move2_p.getEntity().getRadius());
-    Vector<double> const & p1(move1_p.getEntity().getPosition());
-    Vector<double> const & p2(move2_p.getEntity().getPosition());
-    Vector<double> & v1(move1_p.getVector());
-    Vector<double> & v2(move2_p.getVector());
-    double dx(p1.x-p2.x);
-    double dy(p1.y-p2.y);
-    double dvx(v1.x-v2.x);
-    double dvy(v1.y-v2.y);
-    double R(r1+r2);
-
-    double a(dvx*dvx + dvy*dvy);
-    double b(2*dx*dvx + 2*dy*dvy);
-    double c(dx*dx+dy*dy-R*R);
-
-
-    double delta(b*b-4*a*c);
-    if ( delta >= 0.0 )
-    {
-        double t((-b-f_sqrt(delta))/2.0/a);
-        if ( t>= 0.0 && t<= 1.0 ) {
-            move1_p.getVector() = t * move1_p.getVector();
-            move2_p.getVector() = t * move2_p.getVector();
-            return true;
-        } else
-        {
-            return false;
-        }
-    } else
-    {
-        return false;
-    }
-}
+bool checkAndUpdateMoves(Move & move1_p, Move & move2_p);
 
 
 }
